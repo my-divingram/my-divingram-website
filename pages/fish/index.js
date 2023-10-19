22c8992ba0ddab5921611b35c3fad4ed7087a1f2
kajipyman <o.scyllarus0725@gmail.com>
2023-10-19 21:45:15 +0900

    イサキの仲間の追加

--- pages/fish/index.js
+++ pages/fish/index.js
@@ -159,12 +159,12 @@ function Home({data_fish, data_num}) {
                             <h2 className="py-3 text-xs md:text-base text-center text-gray-700 font-medium">タカサゴの仲間</h2>
                         </Link>
                     </div>
-                    {/* <div className="flex justify-center hover:opacity-80">
-                        <Link href="fish/anthias">
-                            <Image src="/img/class/anthias.png" alt="anthias" width={300} height={200} style={{objectFit:"contain"}}/>
-                            <h2 className="py-3 text-xs md:text-base text-center text-gray-700 font-medium">コショウダイの仲間</h2>
+                    <div className="flex justify-center hover:opacity-80">
+                        <Link href="fish/sweetlips">
+                            <Image src="/img/class/sweetlips.jpeg" alt="sweetlips" width={300} height={200} style={{objectFit:"contain"}}/>
+                            <h2 className="py-3 text-xs md:text-base text-center text-gray-700 font-medium">イサキの仲間</h2>
                         </Link>
-                    </div> */}
+                    </div>
                     {/* <div className="flex justify-center hover:opacity-80">
                         <Link href="fish/anthias">
                             <Image src="/img/class/anthias.png" alt="anthias" width={300} height={200} style={{objectFit:"contain"}}/>
@@ -331,7 +331,7 @@ function Home({data_fish, data_num}) {
                         <Link href="fish/angelfish">
                             <Image src="/img/class/angelfish.jpeg" alt="angelfish" width={300} height={200} style={{objectFit:"contain"}}/>
                             <h2 className="py-3 text-xs md:text-base text-center text-gray-700 font-medium">その他</h2>
-                            イワシ、ゴンズイ、ヒメクサアジ、チゴダラ、イタチウオ、ボラ、ダツ、ホウボウ、セミホウボウ、コチ、メギス、タナバタウオ、アゴアマダイ、キントキダイ、ムツ、コバンザメ、 クロサギ、イサキ、テングダイ、アカタチ、タカベ、イシダイ、イシガキダイ、カゴカキダイ、メガネウオ、ツノダシ
+                            イワシ、ゴンズイ、ヒメクサアジ、チゴダラ、イタチウオ、ボラ、ダツ、ホウボウ、セミホウボウ、コチ、メギス、タナバタウオ、アゴアマダイ、キントキダイ、ムツ、コバンザメ、 クロサギ、テングダイ、アカタチ、タカベ、イシダイ、イシガキダイ、カゴカキダイ、メガネウオ、ツノダシ
                         </Link>
                     </div> */}
                 </div>
---
+++ pages/fish/sweetlips/index.js
@@ -0,0 +1,36 @@
+import { client } from "/libs/client";
+import Layout from "/components/Layout";
+import {Family, Genus} from "/components/Class";
+
+// SSG
+export const getStaticProps = async() => {
+	const data_Parapristipoma = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]イサキ属` , limit: 100 }});
+	const data_Plectorhinchus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]コショウダイ属` , limit: 100 }});
+	const data_Diagramma = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]コロダイ属` , limit: 100 }});
+
+	return {
+    	props: {
+    		data_Parapristipoma: data_Parapristipoma.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Plectorhinchus: data_Plectorhinchus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Diagramma: data_Diagramma.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    	},
+	};
+};
+
+export default function Home({data_Parapristipoma, data_Plectorhinchus, data_Diagramma}) {
+
+	return (
+		<Layout title="イサキの仲間">
+			<div className="px-5 md:px-20 bg-gradient-to-b from-white to-sky-100 font-sans">
+
+				<h1 className="pt-10 text-xl md:text-2xl text-center text-sky-800 font-black">イサキの仲間</h1>
+
+				<Family family="イサキ科"></Family>
+				<Genus genus="イサキ属 (Parapristipoma)" data={data_Parapristipoma}></Genus>
+				<Genus genus="コショウダイ属 (Plectorhinchus)" data={data_Plectorhinchus}></Genus>
+				<Genus genus="コロダイ属 (Diagramma)" data={data_Diagramma}></Genus>
+
+			</div>
+		</Layout>
+  	);
+}
