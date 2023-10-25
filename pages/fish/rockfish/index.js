74fc3fac152bf8f9ef47ff55f359eaa6d00f597d
kajipyman <o.scyllarus0725@gmail.com>
2023-10-26 01:34:14 +0900

    掲載順の変更

--- pages/fish/rockfish/index.js
+++ pages/fish/rockfish/index.js
@@ -7,8 +7,11 @@ export const getStaticProps = async() => {
 	const data_Sebastes = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]メバル属` , limit: 100 }});
 	const data_Sebastiscus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]カサゴ属` , limit: 100 }});
 	const data_Pterois = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ミノカサゴ属` , limit: 100 }});
+	const data_Pteropterus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]Pteropterus` , limit: 100 }});
 	const data_Parapterois = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]セトミノカサゴ属` , limit: 100 }});
-	const data_Dendrochirus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ヒメヤマノカミ属` , limit: 100 }});
+	const data_Dendrochirus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]Dendrochirus` , limit: 100 }});
+	const data_Nemapterois = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]Nemapterois` , limit: 100 }});
+	const data_Neochirus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]Neochirus` , limit: 100 }});
 	const data_Taenianotus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ハダカハオコゼ属` , limit: 100 }});
 	const data_Rhinopias = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ボロカサゴ属` , limit: 100 }});
 	const data_Pteroidichthys = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ツノカサゴ属` , limit: 100 }});
@@ -25,8 +28,11 @@ export const getStaticProps = async() => {
     		data_Sebastes: data_Sebastes.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Sebastiscus: data_Sebastiscus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Pterois: data_Pterois.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Pteropterus: data_Pteropterus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Parapterois: data_Parapterois.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Dendrochirus: data_Dendrochirus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Nemapterois: data_Nemapterois.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Neochirus: data_Neochirus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Taenianotus: data_Taenianotus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Rhinopias: data_Rhinopias.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Pteroidichthys: data_Pteroidichthys.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
@@ -41,7 +47,7 @@ export const getStaticProps = async() => {
 	};
 };
 
-export default function Home({data_Sebastes, data_Sebastiscus, data_Pterois, data_Parapterois, data_Dendrochirus, data_Taenianotus, data_Rhinopias, data_Pteroidichthys, data_Scorpaenopsis, data_Scorpaena, data_Sebastapistes, data_Parascorpaena, data_Scorpaenodes, data_Caracanthus, data_Neosebastes}) {
+export default function Home({data_Sebastes, data_Sebastiscus, data_Pterois, data_Pteropterus, data_Parapterois, data_Dendrochirus, data_Nemapterois, data_Neochirus, data_Taenianotus, data_Rhinopias, data_Pteroidichthys, data_Scorpaenopsis, data_Scorpaena, data_Sebastapistes, data_Parascorpaena, data_Scorpaenodes, data_Caracanthus, data_Neosebastes}) {
 
 	return (
 		<Layout title="メバル・カサゴの仲間">
@@ -49,26 +55,29 @@ export default function Home({data_Sebastes, data_Sebastiscus, data_Pterois, dat
 
 				<h1 className="pt-10 text-xl md:text-2xl text-center text-sky-800 font-black">メバル・カサゴの仲間</h1>
 
-				<Family family="メバル科"></Family>
-				<Genus genus="メバル属 (Sebastes)" data={data_Sebastes}></Genus>
-				<Genus genus="カサゴ属 (Sebastiscus)" data={data_Sebastiscus}></Genus>
-
 				<Family family="フサカサゴ科"></Family>
-				<Genus genus="ミノカサゴ属 (Pterois)" data={data_Pterois}></Genus>
-				<Genus genus="セトミノカサゴ属 (Parapterois)" data={data_Parapterois}></Genus>
-				<Genus genus="ヒメヤマノカミ属 (Dendrochirus)" data={data_Dendrochirus}></Genus>
-				<Genus genus="ハダカハオコゼ属 (Taenianotus)" data={data_Taenianotus}></Genus>
-				<Genus genus="ボロカサゴ属 (Rhinopias)" data={data_Rhinopias}></Genus>
-				<Genus genus="ツノカサゴ属 (Pteroidichthys)" data={data_Pteroidichthys}></Genus>
-				<Genus genus="オニカサゴ属 (Scorpaenopsis)" data={data_Scorpaenopsis}></Genus>
-				<Genus genus="フサカサゴ属 (Scorpaena)" data={data_Scorpaena}></Genus>
-				<Genus genus="マダラフサカサゴ属 (Sebastapistes)" data={data_Sebastapistes}></Genus>
-				<Genus genus="ネッタイフサカサゴ属 (Parascorpaena)" data={data_Parascorpaena}></Genus>
-				<Genus genus="イソカサゴ属 (Scorpaenodes)" data={data_Scorpaenodes}></Genus>
 				<Genus genus="ダンゴオコゼ属 (Caracanthus)" data={data_Caracanthus}></Genus>
+				<Genus genus="Dendrochirus属" data={data_Dendrochirus}></Genus>
+				<Genus genus="Nemapterois属" data={data_Nemapterois}></Genus>
+				<Genus genus="Neochirus属" data={data_Neochirus}></Genus>
+				<Genus genus="セトミノカサゴ属 (Parapterois)" data={data_Parapterois}></Genus>
+				<Genus genus="ネッタイフサカサゴ属 (Parascorpaena)" data={data_Parascorpaena}></Genus>
+				<Genus genus="ツノカサゴ属 (Pteroidichthys)" data={data_Pteroidichthys}></Genus>
+				<Genus genus="ミノカサゴ属 (Pterois)" data={data_Pterois}></Genus>
+				<Genus genus="Pteropterus属" data={data_Pteropterus}></Genus>
+				<Genus genus="ボロカサゴ属 (Rhinopias)" data={data_Rhinopias}></Genus>
+				<Genus genus="フサカサゴ属 (Scorpaena)" data={data_Scorpaena}></Genus>
+				<Genus genus="イソカサゴ属 (Scorpaenodes)" data={data_Scorpaenodes}></Genus>
+				<Genus genus="オニカサゴ属 (Scorpaenopsis)" data={data_Scorpaenopsis}></Genus>
+				<Genus genus="マダラフサカサゴ属 (Sebastapistes)" data={data_Sebastapistes}></Genus>
+				<Genus genus="ハダカハオコゼ属 (Taenianotus)" data={data_Taenianotus}></Genus>
 
 				<Family family="ヒレナガカサゴ科"></Family>
 				<Genus genus="ヒレナガカサゴ属 (Neosebastes)" data={data_Neosebastes}></Genus>
+
+				<Family family="メバル科"></Family>
+				<Genus genus="メバル属 (Sebastes)" data={data_Sebastes}></Genus>
+				<Genus genus="カサゴ属 (Sebastiscus)" data={data_Sebastiscus}></Genus>
 
 			</div>
 		</Layout>
