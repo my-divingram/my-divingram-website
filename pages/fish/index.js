e2a7530f3d60f6653768374ff8886cbd265d16a4
kajipyman <o.scyllarus0725@gmail.com>
2023-10-31 14:33:45 +0900

    ベラの仲間の追加

--- pages/fish/index.js
+++ pages/fish/index.js
@@ -247,12 +247,6 @@ function Home({data_fish, data_num}) {
                     {/* <div className="flex justify-center hover:opacity-80">
                         <Link href="fish/angelfish">
                             <Image src="/img/class/angelfish.jpeg" alt="angelfish" width={300} height={200} style={{objectFit:"contain"}}/>
-                            <h2 className="py-3 text-xs md:text-base text-center text-gray-700 font-medium">アイナメ・カジカの仲間</h2>
-                        </Link>
-                    </div> */}
-                    {/* <div className="flex justify-center hover:opacity-80">
-                        <Link href="fish/angelfish">
-                            <Image src="/img/class/angelfish.jpeg" alt="angelfish" width={300} height={200} style={{objectFit:"contain"}}/>
                             <h2 className="py-3 text-xs md:text-base text-center text-gray-700 font-medium">トラギスの仲間</h2>
                         </Link>
                     </div> */}
--- pages/fish/wrasse/index.js
+++ pages/fish/wrasse/index.js
@@ -21,6 +21,23 @@ export const getStaticProps = async() => {
 	const data_Cheilinus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]モチノウオ属` , limit: 100 }});
 	const data_Cheilio = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]カマスベラ属` , limit: 100 }});
 	const data_Thalassoma = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ニシキベラ属` , limit: 100 }});
+	const data_Labroides = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ソメワケベラ属` , limit: 100 }});
+	const data_Hemigymnus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]タレクチベラ属` , limit: 100 }});
+	const data_Parajulis = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]キュウセン属` , limit: 100 }});
+	const data_Cymolutes = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]タテヤマベラ属` , limit: 100 }});
+	const data_Anampses = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ススキベラ属` , limit: 100 }});
+	const data_Pseudocoriss = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]シラタキベラダマシ属` , limit: 100 }});
+	const data_Labrichthys = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]クロベラ属` , limit: 100 }});
+	const data_Novaculops = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]テンスモドキ属` , limit: 100 }});
+	const data_Iniistius = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]テンス属` , limit: 100 }});
+	const data_Hologymnosus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]シロタスキベラ属` , limit: 100 }});
+	const data_Semicossyphus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]コブダイ属` , limit: 100 }});
+	const data_Epibulus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ギチベラ属` , limit: 100 }});
+	const data_Labropsis = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]マナベベラ属` , limit: 100 }});
+	const data_Gomphosus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]クギベラ属` , limit: 100 }});
+	const data_Paracheilinus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]クジャクベラ属` , limit: 100 }});
+	const data_Pseudocheilinus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ニセモチノウオ属` , limit: 100 }});
+	const data_Macropharyngodon = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ノドグロベラ属` , limit: 100 }});
 
 	return {
     	props: {
@@ -41,11 +58,28 @@ export const getStaticProps = async() => {
 			data_Cheilinus: data_Cheilinus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Cheilio: data_Cheilio.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Thalassoma: data_Thalassoma.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+			data_Labroides: data_Labroides.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Hemigymnus: data_Hemigymnus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Parajulis: data_Parajulis.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Cymolutes: data_Cymolutes.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Anampses: data_Anampses.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Pseudocoriss: data_Pseudocoriss.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Labrichthys: data_Labrichthys.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+			data_Novaculops: data_Novaculops.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Iniistius: data_Iniistius.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Hologymnosus: data_Hologymnosus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Semicossyphus: data_Semicossyphus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Epibulus: data_Epibulus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Labropsis: data_Labropsis.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+			data_Gomphosus: data_Gomphosus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+			data_Paracheilinus: data_Paracheilinus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Pseudocheilinus: data_Pseudocheilinus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Macropharyngodon: data_Macropharyngodon.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     	},
 	};
 };
 
-export default function Home({data_Pseudojuloides, data_Terelabrus, data_Pseudolabrus, data_Pteragogus, data_Halichoeres, data_Choerodon, data_Suezichthys, data_Novaculichthys, data_Oxycheilinus, data_Novaculoides, data_Cirrhilabrus, data_Stethojulis, data_Coris, data_Bodianus, data_Cheilinus, data_Cheilio, data_Thalassoma}) {
+export default function Home({data_Pseudojuloides, data_Terelabrus, data_Pseudolabrus, data_Pteragogus, data_Halichoeres, data_Choerodon, data_Suezichthys, data_Novaculichthys, data_Oxycheilinus, data_Novaculoides, data_Cirrhilabrus, data_Stethojulis, data_Coris, data_Bodianus, data_Cheilinus, data_Cheilio, data_Thalassoma, data_Labroides, data_Hemigymnus, data_Parajulis, data_Cymolutes, data_Anampses, data_Pseudocoriss, data_Labrichthys, data_Novaculops, data_Iniistius, data_Hologymnosus, data_Semicossyphus, data_Epibulus, data_Labropsis, data_Gomphosus, data_Paracheilinus, data_Pseudocheilinus, data_Macropharyngodon}) {
 
 	return (
 		<Layout title="ベラの仲間">
@@ -54,23 +88,40 @@ export default function Home({data_Pseudojuloides, data_Terelabrus, data_Pseudol
 				<h1 className="pt-10 text-xl md:text-2xl text-center text-sky-800 font-black">ベラの仲間</h1>
 
 				<Family family="ベラ科"></Family>
-				<Genus genus="カミナリベラ属 (Stethojulis)" data={data_Stethojulis}></Genus>
-				<Genus genus="ササノハベラ属 (Pseudolabrus)" data={data_Pseudolabrus}></Genus>
-				<Genus genus="タキベラ属 (Bodianus)" data={data_Bodianus}></Genus>
-				<Genus genus="モチノウオ属 (Cheilinus)" data={data_Cheilinus}></Genus>
-				<Genus genus="ホンベラ属 (Halichoeres)" data={data_Halichoeres}></Genus>
-				<Genus genus="イトベラ属 (Suezichthys)" data={data_Suezichthys}></Genus>
-				<Genus genus="イトヒキベラ属 (Cirrhilabrus)" data={data_Cirrhilabrus}></Genus>
-				<Genus genus="イラ属 (Choerodon)" data={data_Choerodon}></Genus>
-				<Genus genus="オオヒレテンスモドキ属" data={data_Novaculoides}></Genus>
-				<Genus genus="オグロベラ属 (Pseudojuloides)" data={data_Pseudojuloides}></Genus>
-				<Genus genus="ニシキベラ属 (Thalassoma)" data={data_Thalassoma}></Genus>
-				<Genus genus="オハグロベラ属 (Pteragogus)" data={data_Pteragogus}></Genus>
-				<Genus genus="オビテンスモドキ属 (Novaculichthys)" data={data_Novaculichthys}></Genus>
-				<Genus genus="ホホスジモチノウオ属 (Oxycheilinus)" data={data_Oxycheilinus}></Genus>
 				<Genus genus="カマスベラ属 (Cheilio)" data={data_Cheilio}></Genus>
-				<Genus genus="カンムリベラ属 (Coris)" data={data_Coris}></Genus>
+				<Genus genus="キュウセン属 (Parajulis)" data={data_Parajulis}></Genus>
+				<Genus genus="ギチベラ属 (Epibulus)" data={data_Epibulus}></Genus>
+				<Genus genus="ホホスジモチノウオ属 (Oxycheilinus)" data={data_Oxycheilinus}></Genus>
+				<Genus genus="モチノウオ属 (Cheilinus)" data={data_Cheilinus}></Genus>
+				<Genus genus="イトヒキベラ属 (Cirrhilabrus)" data={data_Cirrhilabrus}></Genus>
+				<Genus genus="クジャクベラ属 (Paracheilinus)" data={data_Paracheilinus}></Genus>
+				<Genus genus="ニセモチノウオ属 (Pseudocheilinus)" data={data_Pseudocheilinus}></Genus>
+				<Genus genus="オハグロベラ属 (Pteragogus)" data={data_Pteragogus}></Genus>
+				<Genus genus="タキベラ属 (Bodianus)" data={data_Bodianus}></Genus>
+				<Genus genus="イラ属 (Choerodon)" data={data_Choerodon}></Genus>
+				<Genus genus="コブダイ属 (Semicossyphus)" data={data_Semicossyphus}></Genus>
 				<Genus genus="ミヤビベラ属 (Terelabrus)" data={data_Terelabrus}></Genus>
+				<Genus genus="ススキベラ属 (Anampses)" data={data_Anampses}></Genus>
+				<Genus genus="カンムリベラ属 (Coris)" data={data_Coris}></Genus>
+				<Genus genus="クギベラ属 (Gomphosus)" data={data_Gomphosus}></Genus>
+				<Genus genus="ホンベラ属 (Halichoeres)" data={data_Halichoeres}></Genus>
+				<Genus genus="タレクチベラ属 (Hemigymnus)" data={data_Hemigymnus}></Genus>
+				<Genus genus="シロタスキベラ属 (Hologymnosus)" data={data_Hologymnosus}></Genus>
+				<Genus genus="クロベラ属 (Labrichthys)" data={data_Labrichthys}></Genus>
+				<Genus genus="ソメワケベラ属 (Labroides)" data={data_Labroides}></Genus>
+				<Genus genus="マナベベラ属 (Labropsis)" data={data_Labropsis}></Genus>
+				<Genus genus="ノドグロベラ属 (Macropharyngodon)" data={data_Macropharyngodon}></Genus>
+				<Genus genus="シラタキベラダマシ属 (Pseudocoris)" data={data_Pseudocoriss}></Genus>
+				<Genus genus="オグロベラ属 (Pseudojuloides)" data={data_Pseudojuloides}></Genus>
+				<Genus genus="カミナリベラ属 (Stethojulis)" data={data_Stethojulis}></Genus>
+				<Genus genus="ニシキベラ属 (Thalassoma)" data={data_Thalassoma}></Genus>
+				<Genus genus="タテヤマベラ属 (Cymolutes)" data={data_Cymolutes}></Genus>
+				<Genus genus="テンス属 (Iniistius)" data={data_Iniistius}></Genus>
+				<Genus genus="オビテンスモドキ属 (Novaculichthys)" data={data_Novaculichthys}></Genus>
+				<Genus genus="オオヒレテンスモドキ属 (Novaculoides)" data={data_Novaculoides}></Genus>
+				<Genus genus="テンスモドキ属 (Novaculops)" data={data_Novaculops}></Genus>
+				<Genus genus="ササノハベラ属 (Pseudolabrus)" data={data_Pseudolabrus}></Genus>
+				<Genus genus="イトベラ属 (Suezichthys)" data={data_Suezichthys}></Genus>
 
 			</div>
 		</Layout>
