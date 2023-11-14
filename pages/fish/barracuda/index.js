9220a5aebf7acaef978406428b8c916a5eb8cc9c
kajipyman <o.scyllarus0725@gmail.com>
2023-11-14 20:31:05 +0900

    カマスの仲間の追加

---
+++ pages/fish/barracuda/index.js
@@ -0,0 +1,33 @@
+import { client } from "/libs/client";
+import Layout from "/components/Layout";
+import {Family, Genus} from "/components/Class";
+
+// SSG
+export const getStaticProps = async() => {
+	const data = await client.get({ endpoint: "uwphoto", queries: { filters: `class[equals]barracuda` , limit: 1 }});
+	const data_Sphyraena = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]カマス属` , limit: 100 }});
+
+	return {
+    	props: {
+			data_num: data.totalCount,
+    		data_Sphyraena: data_Sphyraena.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    	},
+	};
+};
+
+export default function Home({data_num, data_Sphyraena}) {
+
+	return (
+		<Layout title="カマスの仲間">
+			<div className="px-5 md:px-20 bg-gradient-to-b from-white to-sky-100 font-sans">
+
+				<h1 className="pt-10 text-xl md:text-2xl text-center text-sky-800 font-black">カマスの仲間</h1>
+				<p className="pt-2 text-xs md:text-sm text-center text-gray-700 font-medium">掲載種 : {data_num}種</p>
+
+				<Family family="カマス科"></Family>
+				<Genus genus="カマス属 (Sphyraena)" data={data_Sphyraena}></Genus>
+
+			</div>
+		</Layout>
+  	);
+}
--- pages/fish/index.js
+++ pages/fish/index.js
@@ -298,12 +298,12 @@ function Home({data_fish, data_num}) {
                             <h2 className="py-3 text-xs md:text-base text-center text-gray-700 font-medium">ニザダイの仲間</h2>
                         </Link>
                     </div>
-                    {/* <div className="flex justify-center hover:opacity-80">
-                        <Link href="fish/angelfish">
-                            <Image src="/img/class/angelfish.jpeg" alt="angelfish" width={300} height={200} style={{objectFit:"contain"}}/>
+                    <div className="flex justify-center hover:opacity-80">
+                        <Link href="fish/barracuda">
+                            <Image src="/img/class/barracuda.png" alt="barracuda" width={300} height={200} style={{objectFit:"contain"}}/>
                             <h2 className="py-3 text-xs md:text-base text-center text-gray-700 font-medium">カマスの仲間</h2>
                         </Link>
-                    </div> */}
+                    </div>
                     {/* <div className="flex justify-center hover:opacity-80">
                         <Link href="fish/angelfish">
                             <Image src="/img/class/angelfish.jpeg" alt="angelfish" width={300} height={200} style={{objectFit:"contain"}}/>
