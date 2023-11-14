317423f99f1db05110c36eea3032af291e2c124d
kajipyman <o.scyllarus0725@gmail.com>
2023-11-14 22:35:51 +0900

    カレイの仲間の追加

---
+++ pages/fish/flatfish/index.js
@@ -0,0 +1,66 @@
+import { client } from "/libs/client";
+import Layout from "/components/Layout";
+import {Family, Genus} from "/components/Class";
+
+// SSG
+export const getStaticProps = async() => {
+	const data = await client.get({ endpoint: "uwphoto", queries: { filters: `class[equals]seachub` , limit: 1 }});
+	const data_Paralichthys = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ヒラメ属` , limit: 100 }});
+	const data_Asterorhombus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]セイテンビラメ属` , limit: 100 }});
+	const data_Bothus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]トゲダルマガレイ属` , limit: 100 }});
+	const data_Crossorhombus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]コウベダルマガレイ属` , limit: 100 }});
+	const data_Engyprosopon = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ダルマガレイ属` , limit: 100 }});
+	const data_Pleuronichthys = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]メイタガレイ属` , limit: 100 }});
+	const data_Aseraggodes = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]トビササウシノシタ属` , limit: 100 }});
+	const data_Brachirus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ミナミシマウシノシタ属` , limit: 100 }});
+	const data_Pardachirus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ミナミウシノシタ属` , limit: 100 }});
+	const data_Soleichthys = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]サザナミウシノシタ属` , limit: 100 }});
+
+	return {
+    	props: {
+			data_num: data.totalCount,
+    		data_Paralichthys: data_Paralichthys.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Asterorhombus: data_Asterorhombus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Bothus: data_Bothus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Crossorhombus: data_Crossorhombus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Engyprosopon: data_Engyprosopon.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Pleuronichthys: data_Pleuronichthys.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Aseraggodes: data_Aseraggodes.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Brachirus: data_Brachirus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Pardachirus: data_Pardachirus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Soleichthys: data_Soleichthys.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    	},
+	};
+};
+
+export default function Home({data_num, data_Paralichthys, data_Asterorhombus, data_Bothus, data_Crossorhombus, data_Engyprosopon, data_Pleuronichthys, data_Aseraggodes, data_Brachirus, data_Pardachirus, data_Soleichthys}) {
+
+	return (
+		<Layout title="カレイの仲間">
+			<div className="px-5 md:px-20 bg-gradient-to-b from-white to-sky-100 font-sans">
+
+				<h1 className="pt-10 text-xl md:text-2xl text-center text-sky-800 font-black">カレイの仲間</h1>
+				<p className="pt-2 text-xs md:text-sm text-center text-gray-700 font-medium">掲載種 : {data_num}種</p>
+
+				<Family family="ヒラメ科"></Family>
+				<Genus genus="ヒラメ属 (Paralichthys)" data={data_Paralichthys}></Genus>
+
+				<Family family="ダルマガレイ科"></Family>
+				<Genus genus="セイテンビラメ属 (Asterorhombus)" data={data_Asterorhombus}></Genus>
+				<Genus genus="トゲダルマガレイ属 (Bothus)" data={data_Bothus}></Genus>
+				<Genus genus="コウベダルマガレイ属 (Crossorhombus)" data={data_Crossorhombus}></Genus>
+				<Genus genus="ダルマガレイ属 (Engyprosopon)" data={data_Engyprosopon}></Genus>
+
+				<Family family="カレイ科"></Family>
+				<Genus genus="メイタガレイ属 (Pleuronichthys)" data={data_Pleuronichthys}></Genus>
+
+				<Family family="ササウシノシタ科"></Family>
+				<Genus genus="トビササウシノシタ属 (Aseraggodes)" data={data_Aseraggodes}></Genus>
+				<Genus genus="ミナミシマウシノシタ属 (Brachirus)" data={data_Brachirus}></Genus>
+				<Genus genus="ミナミウシノシタ属 (Pardachirus)" data={data_Pardachirus}></Genus>
+				<Genus genus="サザナミウシノシタ属 (Soleichthys)" data={data_Soleichthys}></Genus>
+
+			</div>
+		</Layout>
+  	);
+}
