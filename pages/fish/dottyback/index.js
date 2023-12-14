46e751302f1aef96fdfa91b33376ad7d213314b2
kajipyman <o.scyllarus0725@gmail.com>
2023-12-14 22:39:03 +0900

    メギスの仲間の追加

---
+++ pages/fish/dottyback/index.js
@@ -0,0 +1,39 @@
+import { client } from "/libs/client";
+import Layout from "/components/Layout";
+import {Family, Genus} from "/components/Class";
+
+// SSG
+export const getStaticProps = async() => {
+	const data = await client.get({ endpoint: "uwphoto", queries: { filters: `class[equals]dottyback` , limit: 1 }});
+	const data_Labracinus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]メギス属` , limit: 100 }});
+	const data_Pictichromis = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]クレナイニセスズメ属` , limit: 100 }});
+	const data_Pseudochromis = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ニセスズメ属` , limit: 100 }});
+
+	return {
+    	props: {
+			data_num: data.totalCount,
+    		data_Pseudochromis: data_Pseudochromis.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+			data_Pictichromis: data_Pictichromis.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+			data_Labracinus: data_Labracinus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    	},
+	};
+};
+
+export default function Home({data_num, data_Labracinus, data_Pictichromis, data_Pseudochromis, data_Monotaxis}) {
+
+	return (
+		<Layout title="メギスの仲間">
+			<div className="px-5 md:px-20 bg-gradient-to-b from-white to-sky-100 font-sans">
+
+				<h1 className="pt-10 text-xl md:text-2xl text-center text-sky-800 font-black">メギスの仲間</h1>
+				<p className="pt-2 text-xs md:text-sm text-center text-gray-700 font-medium">掲載種 : {data_num}種</p>
+
+				<Family family="メギス科"></Family>
+				<Genus genus="メギス属 (Labracinus)" data={data_Labracinus}></Genus>
+				<Genus genus="クレナイニセスズメ属 (Pictichromis)" data={data_Pictichromis}></Genus>
+				<Genus genus="ニセスズメ属 (Pseudochromis)" data={data_Pseudochromis}></Genus>
+
+			</div>
+		</Layout>
+  	);
+}
--- pages/fish/index.js
+++ pages/fish/index.js
@@ -137,6 +137,12 @@ function Home({data_fish, data_num}) {
                         </Link>
                     </div>
                     <div className="flex justify-center hover:opacity-80">
+                        <Link href="fish/dottyback">
+                            <Image src="/img/class/dottyback.jpeg" alt="dottyback" width={300} height={200} style={{objectFit:"contain"}}/>
+                            <h2 className="py-3 text-xs md:text-base text-center text-gray-700 font-medium">メギスの仲間</h2>
+                        </Link>
+                    </div>
+                    <div className="flex justify-center hover:opacity-80">
                         <Link href="fish/cardinalfish">
                             <Image src="/img/class/cardinalfish.jpeg" alt="cardinalfish" width={300} height={200} style={{objectFit:"contain"}}/>
                             <h2 className="py-3 text-xs md:text-base text-center text-gray-700 font-medium">テンジクダイの仲間</h2>
