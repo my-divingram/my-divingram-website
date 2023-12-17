d27ca7d13aeb84f215bdd5c8026ffa94c3aacc8a
kajipyman <o.scyllarus0725@gmail.com>
2023-12-18 00:16:25 +0900

    Google Analyticsへの対応

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,40 @@
 import '@/styles/globals.css'
+import * as gtag from "/libs/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
+  return (
+  <>
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+    />
+    <Script
+      id="gtag-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+        `,
+      }}
+    />
+    <Component {...pageProps} />
+  </>)
 }
