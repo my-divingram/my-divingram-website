1eb0ce550f674663f631355a6e746b1fd01aff82
kajipyman <o.scyllarus0725@gmail.com>
2024-06-03 21:16:31 +0900

    メシマウバウオ属の追加

--- pages/fish/clingfish/index.js
+++ pages/fish/clingfish/index.js
@@ -10,6 +10,7 @@ export const getStaticProps = async() => {
 	const data_Conidens = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]アンコウウバウオ属` , limit: 100 }});
 	const data_Diademichthys = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ハシナガウバウオ属` , limit: 100 }});
 	const data_Discotrema = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ウミシダウバウオ属` , limit: 100 }});
+	const data_Pherallodichthys = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]メシマウバウオ属` , limit: 100 }});
 
 	return {
     	props: {
@@ -19,11 +20,12 @@ export const getStaticProps = async() => {
     		data_Conidens: data_Conidens.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Diademichthys: data_Diademichthys.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
 			data_Discotrema: data_Discotrema.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+			data_Pherallodichthys: data_Pherallodichthys.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     	},
 	};
 };
 
-export default function Home({data_num, data_Propherallodus, data_Lepadichthys, data_Conidens, data_Diademichthys, data_Discotrema}) {
+export default function Home({data_num, data_Propherallodus, data_Lepadichthys, data_Conidens, data_Diademichthys, data_Discotrema, data_Pherallodichthys}) {
 
 	return (
 		<Layout title="ウバウオの仲間">
@@ -37,6 +39,7 @@ export default function Home({data_num, data_Propherallodus, data_Lepadichthys, 
 				<Genus genus="ハシナガウバウオ属 (Diademichthys)" data={data_Diademichthys}></Genus>
 				<Genus genus="ウミシダウバウオ属 (Discotrema)" data={data_Discotrema}></Genus>
 				<Genus genus="ミサキウバウオ属 (Lepadichthys)" data={data_Lepadichthys}></Genus>
+				<Genus genus="メシマウバウオ属 (Pherallodichthys)" data={data_Pherallodichthys}></Genus>
 				<Genus genus="ヒメウバウオ属 (Propherallodus)" data={data_Propherallodus}></Genus>
 
 			</div>
