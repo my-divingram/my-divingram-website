11bdeeef32ac0a33c7039c29f9ede15eff32c8ae
kajipyman <o.scyllarus0725@gmail.com>
2024-07-01 23:25:59 +0900

    チダイ属の追加

--- pages/fish/seabream/index.js
+++ pages/fish/seabream/index.js
@@ -7,17 +7,19 @@ export const getStaticProps = async() => {
 	const data = await client.get({ endpoint: "uwphoto", queries: { filters: `class[equals]seabream` , limit: 1 }});
 	const data_Pagrus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]マダイ属` , limit: 100 }});
 	const data_Acanthopagrus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]クロダイ属` , limit: 100 }});
+	const data_Evynnis = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]チダイ属` , limit: 100 }});
 
 	return {
     	props: {
 			data_num: data.totalCount,
     		data_Pagrus: data_Pagrus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Acanthopagrus: data_Acanthopagrus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Evynnis: data_Evynnis.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     	},
 	};
 };
 
-export default function Home({data_num, data_Pagrus, data_Acanthopagrus}) {
+export default function Home({data_num, data_Pagrus, data_Acanthopagrus, data_Evynnis}) {
 
 	return (
 		<Layout title="タイの仲間">
@@ -28,6 +30,7 @@ export default function Home({data_num, data_Pagrus, data_Acanthopagrus}) {
 
 				<Family family="タイ科"></Family>
 				<Genus genus="クロダイ属 (Acanthopagrus)" data={data_Acanthopagrus}></Genus>
+				<Genus genus="チダイ属 (Evynnis)" data={data_Evynnis}></Genus>
 				<Genus genus="マダイ属 (Pagrus)" data={data_Pagrus}></Genus>
 
 			</div>
