b7d600c9bfa014336c695237c47cce85924a8597
kajipyman <o.scyllarus0725@gmail.com>
2025-07-02 00:00:34 +0900

    Oxylebius属の追加

--- pages/fish/others/index.js
+++ pages/fish/others/index.js
@@ -53,6 +53,7 @@ export const getStaticProps = async() => {
 	const data_Sardinops = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]マイワシ属` , limit: 100 }});
 	const data_Cheilotrema = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]Cheilotrema` , limit: 100 }});
 	const data_Embiotoca = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]Embiotoca` , limit: 100 }});
+	const data_Oxylebius = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]Oxylebius` , limit: 100 }});
 
 	return {
     	props: {
@@ -105,11 +106,12 @@ export const getStaticProps = async() => {
     		data_Sardinops: data_Sardinops.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Cheilotrema: data_Cheilotrema.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Embiotoca: data_Embiotoca.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Oxylebius: data_Oxylebius.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     	},
 	};
 };
 
-export default function Home({data_num, data_Acanthocepola, data_Oplegnathus, data_Hexagrammos, data_Phtheirichthys, data_Gerres, data_Plotosus, data_Spratelloides, data_Brotula, data_Microcanthus, data_Tylosurus, data_Trachipterus, data_Dactyloptena, data_Labracoglossa, data_Lotella, data_Chelidonichthys, data_Metavelifer, data_Scombrops, data_Mugil, data_Mola, data_Echeneis, data_Evistias, data_Nemichthys, data_Trichiurus, data_Psenes, data_Zeus, data_Alepisaurus, data_Physiculus, data_Kuhlia, data_Lepidotrigla, data_Zoarchias, data_Eumicrotremus, data_Liparis, data_Pholidichthys, data_Hypoatherina, data_Lateolabrax, data_Ditrema, data_Monodactylus, data_Diaphus, data_Muraenesox, data_Halophryne, data_Herklotsichthys, data_Strongylura, data_Cepola, data_Terapon, data_Plicomugil, data_Sardinops, data_Cheilotrema, data_Embiotoca}) {
+export default function Home({data_num, data_Acanthocepola, data_Oplegnathus, data_Hexagrammos, data_Phtheirichthys, data_Gerres, data_Plotosus, data_Spratelloides, data_Brotula, data_Microcanthus, data_Tylosurus, data_Trachipterus, data_Dactyloptena, data_Labracoglossa, data_Lotella, data_Chelidonichthys, data_Metavelifer, data_Scombrops, data_Mugil, data_Mola, data_Echeneis, data_Evistias, data_Nemichthys, data_Trichiurus, data_Psenes, data_Zeus, data_Alepisaurus, data_Physiculus, data_Kuhlia, data_Lepidotrigla, data_Zoarchias, data_Eumicrotremus, data_Liparis, data_Pholidichthys, data_Hypoatherina, data_Lateolabrax, data_Ditrema, data_Monodactylus, data_Diaphus, data_Muraenesox, data_Halophryne, data_Herklotsichthys, data_Strongylura, data_Cepola, data_Terapon, data_Plicomugil, data_Sardinops, data_Cheilotrema, data_Embiotoca, data_Oxylebius}) {
 
 	return (
 		<Layout title="その他の海水魚 | 僕らむの魚図鑑" description="その他の海水魚の一覧です" url="https://www.my-divingram.com/fish/others" imageUrl="https://www.my-divingram.com/img/class/others.jpeg">
@@ -194,6 +196,7 @@ export default function Home({data_num, data_Acanthocepola, data_Oplegnathus, da
 				{/* 1856 */}
 				<Family family="アイナメ科"></Family>
 				<Genus genus="アイナメ属 (Hexagrammos)" data={data_Hexagrammos}></Genus>
+				<Genus genus="Oxylebius属" data={data_Oxylebius}></Genus>
 
 				{/* 2075 */}
 				<Family family="クサウオ科"></Family>
