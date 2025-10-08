80c14c206e5a0fe5d64ce16e089930fa68332036
kajipyman <o.scyllarus0725@gmail.com>
2025-10-08 15:35:46 +0900

    ノコギリハゼ属の追加

--- pages/fish/freshwaterfish/index.js
+++ pages/fish/freshwaterfish/index.js
@@ -26,7 +26,7 @@ export const getStaticProps = async() => {
 	const data_Gasterosteus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]イトヨ属` , limit: 100 }});
 	const data_Sarcocheilichthys = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ヒガイ属` , limit: 100 }});
 	const data_Gnathopogon = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]タモロコ属` , limit: 100 }});
-	const data_Eleotris = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]カワアナゴ属[and]class[equals]freshwaterfish` , limit: 100 }});
+	const data_Eleotris = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]カワアナゴ属` , limit: 100 }});
 	const data_Rhynchocypris = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]アブラハヤ属` , limit: 100 }});
 	const data_Plecoglossus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]アユ属` , limit: 100 }});
 	const data_Tachysurus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ギバチ属` , limit: 100 }});
@@ -38,6 +38,7 @@ export const getStaticProps = async() => {
 	const data_Poecilia = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]グッピー属` , limit: 100 }});
 	const data_Stenogobius = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]タネカワハゼ属` , limit: 100 }});
 	const data_Rhyacichthys = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ツバサハゼ属` , limit: 100 }});
+	const data_Butis = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ノコギリハゼ属` , limit: 100 }});
 
 	return {
     	props: {
@@ -75,11 +76,12 @@ export const getStaticProps = async() => {
     		data_Poecilia: data_Poecilia.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Stenogobius: data_Stenogobius.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Rhyacichthys: data_Rhyacichthys.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Butis: data_Butis.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     	},
 	};
 };
 
-export default function Home({data_num, data_Candidia, data_Opsariichthys, data_Rhinogobius, data_Tridentiger, data_Sicyopus, data_Anguilla, data_Oreochromis, data_Xiphophorus, data_Stiphodon, data_Sicyopterus, data_Lentipes, data_Micropterus, data_Lepomis, data_Oncorhynchus, data_Pseudaspius, data_Gymnogobius, data_Cottus, data_Lethenteron, data_Gasterosteus, data_Sarcocheilichthys, data_Gnathopogon, data_Rhynchocypris, data_Eleotris, data_Plecoglossus, data_Tachysurus, data_Pseudogobio, data_Silurus, data_Cobitis, data_Pungtungia, data_Microphis, data_Poecilia, data_Stenogobius, data_Rhyacichthys}) {
+export default function Home({data_num, data_Candidia, data_Opsariichthys, data_Rhinogobius, data_Tridentiger, data_Sicyopus, data_Anguilla, data_Oreochromis, data_Xiphophorus, data_Stiphodon, data_Sicyopterus, data_Lentipes, data_Micropterus, data_Lepomis, data_Oncorhynchus, data_Pseudaspius, data_Gymnogobius, data_Cottus, data_Lethenteron, data_Gasterosteus, data_Sarcocheilichthys, data_Gnathopogon, data_Rhynchocypris, data_Eleotris, data_Plecoglossus, data_Tachysurus, data_Pseudogobio, data_Silurus, data_Cobitis, data_Pungtungia, data_Microphis, data_Poecilia, data_Stenogobius, data_Rhyacichthys, data_Butis}) {
 
 	return (
 		<Layout title="淡水魚 | 僕らむの魚図鑑" description="淡水魚の一覧です" url="https://www.my-divingram.com/fish/freshwaterfish" imageUrl="https://www.my-divingram.com/img/class/freshwaterfish.jpeg">
@@ -167,6 +169,8 @@ export default function Home({data_num, data_Candidia, data_Opsariichthys, data_
 				<Genus genus="ツバサハゼ属 (Rhyacichthys)" data={data_Rhyacichthys}></Genus>
 
 				<Family family="カワアナゴ科"></Family>
+				{/* 3719 */}
+				<Genus genus="ノコギリハゼ属 (Butis)" data={data_Butis}></Genus>
 				{/* 3723 */}
 				<Genus genus="カワアナゴ属 (Eleotris)" data={data_Eleotris}></Genus>
 
