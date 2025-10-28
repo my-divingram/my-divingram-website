404e29d018039fb3a2eb3dee633e0bdf08f033b3
kajipyman <o.scyllarus0725@gmail.com>
2025-10-29 01:57:08 +0900

    フサギンポ属の追加

--- pages/fish/others/index.js
+++ pages/fish/others/index.js
@@ -64,6 +64,7 @@ export const getStaticProps = async() => {
 	const data_Anarhichas = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]オオカミウオ属` , limit: 100 }});
 	const data_Opisthocentrus = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]オキカズナギ属` , limit: 100 }});
 	const data_Rhodymenichthys = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]ハコダテギンポ属` , limit: 100 }});
+	const data_Chirolophis = await client.get({ endpoint: "uwphoto", queries: { filters: `genus[equals]フサギンポ属` , limit: 100 }});
 
 	return {
     	props: {
@@ -127,11 +128,12 @@ export const getStaticProps = async() => {
     		data_Anarhichas: data_Anarhichas.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Opisthocentrus: data_Opisthocentrus.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     		data_Rhodymenichthys: data_Rhodymenichthys.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
+    		data_Chirolophis: data_Chirolophis.contents.sort((a, b) => a.japaneseName.localeCompare(b.japaneseName), "ja"),
     	},
 	};
 };
 
-export default function Home({data_num, data_Acanthocepola, data_Oplegnathus, data_Hexagrammos, data_Phtheirichthys, data_Gerres, data_Plotosus, data_Spratelloides, data_Brotula, data_Microcanthus, data_Tylosurus, data_Trachipterus, data_Dactyloptena, data_Labracoglossa, data_Lotella, data_Chelidonichthys, data_Metavelifer, data_Scombrops, data_Mugil, data_Mola, data_Echeneis, data_Evistias, data_Nemichthys, data_Trichiurus, data_Psenes, data_Zeus, data_Alepisaurus, data_Physiculus, data_Kuhlia, data_Lepidotrigla, data_Zoarchias, data_Eumicrotremus, data_Liparis, data_Pholidichthys, data_Hypoatherina, data_Lateolabrax, data_Ditrema, data_Monodactylus, data_Diaphus, data_Muraenesox, data_Halophryne, data_Herklotsichthys, data_Strongylura, data_Cepola, data_Terapon, data_Plicomugil, data_Sardinops, data_Cheilotrema, data_Embiotoca, data_Oxylebius, data_Sillago, data_Dictyosoma, data_Planiliza, data_Sardinella, data_Moolgarda, data_Ammodytoides, data_Atherinomorus, data_Anarhichas, data_Opisthocentrus, data_Rhodymenichthys}) {
+export default function Home({data_num, data_Acanthocepola, data_Oplegnathus, data_Hexagrammos, data_Phtheirichthys, data_Gerres, data_Plotosus, data_Spratelloides, data_Brotula, data_Microcanthus, data_Tylosurus, data_Trachipterus, data_Dactyloptena, data_Labracoglossa, data_Lotella, data_Chelidonichthys, data_Metavelifer, data_Scombrops, data_Mugil, data_Mola, data_Echeneis, data_Evistias, data_Nemichthys, data_Trichiurus, data_Psenes, data_Zeus, data_Alepisaurus, data_Physiculus, data_Kuhlia, data_Lepidotrigla, data_Zoarchias, data_Eumicrotremus, data_Liparis, data_Pholidichthys, data_Hypoatherina, data_Lateolabrax, data_Ditrema, data_Monodactylus, data_Diaphus, data_Muraenesox, data_Halophryne, data_Herklotsichthys, data_Strongylura, data_Cepola, data_Terapon, data_Plicomugil, data_Sardinops, data_Cheilotrema, data_Embiotoca, data_Oxylebius, data_Sillago, data_Dictyosoma, data_Planiliza, data_Sardinella, data_Moolgarda, data_Ammodytoides, data_Atherinomorus, data_Anarhichas, data_Opisthocentrus, data_Rhodymenichthys, data_Chirolophis}) {
 
 	return (
 		<Layout title="その他の海水魚 | 僕らむの魚図鑑" description="その他の海水魚の一覧です" url="https://www.my-divingram.com/fish/others" imageUrl="https://www.my-divingram.com/img/class/others.jpeg">
@@ -303,6 +305,7 @@ export default function Home({data_num, data_Acanthocepola, data_Oplegnathus, da
 
 				{/* 3427 */}
 				<Family family="タウエガジ科"></Family>
+				<Genus genus="フサギンポ属 (Chirolophis)" data={data_Chirolophis}></Genus>
 				<Genus genus="ダイナンギンポ属 (Dictyosoma)" data={data_Dictyosoma}></Genus>
 				<Genus genus="オキカズナギ属 (Opisthocentrus)" data={data_Opisthocentrus}></Genus>
 				<Genus genus="カズナギ属 (Zoarchias)" data={data_Zoarchias}></Genus>
