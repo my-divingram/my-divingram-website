2437840e3b194c561fab43a211ef5392bc447f50
kajipyman <o.scyllarus0725@gmail.com>
2025-12-02 02:08:45 +0900

    グループアイコンに代表ポイントを表示

--- components/LocationMap.js
+++ components/LocationMap.js
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster';
@@ -6,19 +6,16 @@ import React from 'react';
 
 function MapController({ center, zoom }) {
     const map = useMap();
-
     React.useEffect(() => {
         if (center) {
             const targetZoom = (zoom !== undefined && zoom !== null) ? zoom : map.getZoom();
             map.setView(center, targetZoom);
         }
     }, [center, zoom, map]);
-
     return null;
 }
 
 export default function LocationMap({ markers, onMarkerClick, center, zoom }) {
-
     const defaultCenter = [35.6809591, 139.7673068];
     const defaultZoom = 4;
 
@@ -26,40 +23,53 @@ export default function LocationMap({ markers, onMarkerClick, center, zoom }) {
         return <p>地図データを読み込めません。</p>;
     }
 
+    const markersByLocation = new Map(markers.map(m => [m.location, m]));
+
     const createClusterCustomIcon = (cluster) => {
         const childMarkers = cluster.getAllChildMarkers();
         const uniqueSpecies = new Set();
-        const markersMap = new Map(
-            markers.map(m => [`${m.lat}_${m.lng}`, m.speciesIds])
-        );
+        let maxCount = -1;
+        let topLocation = "";
 
         childMarkers.forEach(marker => {
-            const latLngKey = `${marker.getLatLng().lat}_${marker.getLatLng().lng}`;
-            const latestSpeciesIds = markersMap.get(latLngKey);
+            const locationName = marker.options.title;
+            const data = markersByLocation.get(locationName);
 
-            if (latestSpeciesIds) {
-                latestSpeciesIds.forEach(id => uniqueSpecies.add(id));
+            if (data) {
+                data.speciesIds.forEach(id => uniqueSpecies.add(id));
+                if (data.speciesCount > maxCount) {
+                    maxCount = data.speciesCount;
+                    topLocation = data.location;
+                }
             }
         });
 
-        const count = uniqueSpecies.size;
+        const totalCount = uniqueSpecies.size;
+        const displayName = topLocation || "Area";
 
         let c = ' marker-cluster-';
-        if (count < 100) { c += 'small'; }
-        else if (count < 500) { c += 'medium'; }
+        if (totalCount < 100) { c += 'small'; }
+        else if (totalCount < 500) { c += 'medium'; }
         else { c += 'large'; }
 
         return L.divIcon({
-            html: `<div><span>${count}</span></div>`,
+            html: `
+                <div>
+                    <span style="font-size: 13px; font-weight: 800; display: block;">${totalCount}</span>
+                    <span style="font-size: 10px; font-weight: 600; margin-top: 0px; display: block; white-space: nowrap; max-width: 100px; overflow: hidden; text-overflow: ellipsis;">
+                        ${displayName}${totalCount > 1 ? 'など' : ''}
+                    </span>
+                </div>
+            `,
             className: 'marker-cluster' + c,
-            iconSize: new L.Point(40, 40)
+            iconSize: L.point(0, 0),
+            iconAnchor: [0, 0]
         });
     };
 
     const createSinglePinIcon = (marker) => {
         const { location, speciesCount, isDimmed } = marker;
         const opacityStyle = isDimmed ? 'opacity: 0.6;' : 'opacity: 1;';
-
        return L.divIcon({
             html: `<div class="custom-single-pin" style="transform: translate(-50%, -50%); ${opacityStyle}">${location}<span>${speciesCount}種</span></div>`,
             className: '',
@@ -75,14 +85,11 @@ export default function LocationMap({ markers, onMarkerClick, center, zoom }) {
             className="w-full h-[300px] md:h-[400px]"
             worldCopyJump={true}
         >
-
             <MapController center={center} zoom={zoom} />
-
             <TileLayer
                 url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                attribution='&copy; CARTO'
             />
-
             <MarkerClusterGroup
                 spiderfyOnMaxZoom={true}
                 showCoverageOnHover={false}
@@ -92,15 +99,11 @@ export default function LocationMap({ markers, onMarkerClick, center, zoom }) {
                     <Marker
                         key={marker.location}
                         position={[marker.lat, marker.lng]}
-                        eventHandlers={{
-                            click: () => {
-                                onMarkerClick(marker.location);
-                            },
-                        }}
+                        title={marker.location}
+                        eventHandlers={{ click: () => onMarkerClick(marker.location) }}
                         icon={createSinglePinIcon(marker)}
                         zIndexOffset={marker.isDimmed ? 0 : 1000}
-                    >
-                    </Marker>
+                    />
                 ))}
             </MarkerClusterGroup>
         </MapContainer>
