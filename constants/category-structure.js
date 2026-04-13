e229891788cabdd9fd5d0a3d32c044d0a555df60
kajipyman <o.scyllarus0725@gmail.com>
2026-04-14 00:37:07 +0900

    フナ属の追加

--- constants/category-structure.js
+++ constants/category-structure.js
@@ -176,6 +176,7 @@ export const categoryStructure = {
             familyName: "コイ科",
             genusName: [
                 { key: "タナゴ属", displayName: "タナゴ属 (Acheilognathus)" },
+                { key: "フナ属", displayName: "フナ属 (Carassius)" },
                 { key: "コイ属", displayName: "コイ属 (Cyprinus)" },
                 { key: "タモロコ属", displayName: "タモロコ属 (Gnathopogon)" },
                 { key: "カマツカ属", displayName: "カマツカ属 (Pseudogobio)" },
