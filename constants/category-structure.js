4bca1f2e2fa56d9cbbd13272e1908cb2b8c46557
kajipyman <o.scyllarus0725@gmail.com>
2026-08-05 15:39:45 +0900

    サバ属の追加

--- constants/category-structure.js
+++ constants/category-structure.js
@@ -1442,6 +1442,7 @@ export const categoryStructure = {
                 { key: "ニジョウサバ属", displayName: "ニジョウサバ属 (Grammatorcynus)" },
                 { key: "イソマグロ属", displayName: "イソマグロ属 (Gymnosarda)" },
                 { key: "グルクマ属", displayName: "グルクマ属 (Rastrelliger)" },
+                { key: "サバ属", displayName: "サバ属 (Scomber)" },
                 { key: "サワラ属", displayName: "サワラ属 (Scomberomorus)" },
             ]
         }
